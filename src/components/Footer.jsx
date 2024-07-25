716641b66c5ece910b43c9f7ca9122f2a9478b92
ShubhamGavand1234 <shubhamgavand@yahoo.in>
2024-07-25 16:06:20 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Footer() {
+    const year = new Date().getFullYear();
+return(
+    <footer>
+    <p>Copyright ⓒ {year}</p>
+    </footer>)
+}
+
+export default Footer;
